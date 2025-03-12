479aaeafdb01af952c1c388dd0bc520fc36aa2fb
8006169054 <43157353+8006169054@users.noreply.github.com>
2025-03-12 11:05:40 +0900

    grid 추가

--- src/main/java/kc/logix/common/view/PagePaths.java
+++ src/main/java/kc/logix/common/view/PagePaths.java
@@ -13,6 +13,7 @@ public class PagePaths {
 	public static Map<String, PageInfo> links = new HashMap<>();
 	static {
 		PagePaths.links.put("/HOME", PageInfo.builder().linkPath("/apps/main/home").auth(true).build());
+		PagePaths.links.put("/SAMPLE/GRID1", PageInfo.builder().linkPath("/apps/sample/grid1").auth(true).build());
 	}
 	
 	@Data
